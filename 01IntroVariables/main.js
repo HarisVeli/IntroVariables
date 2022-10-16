7a212d368ef9e0ce13f0537b342b23aa852f3854
HarisVeli <haroldas.velicka@gmail.com>
2022-10-16 15:34:31 -0500

    adding first js file

---
+++ 01IntroVariables/main.js
@@ -0,0 +1,24 @@
+      // cia rasomas js kode
+      5 + 5;
+
+      // sukuriame kintmaji
+      // = priskiria desne puse prie kairiai
+      let vardas = "James";
+
+      const h1El = document.createElement('h1');
+      // pridedam jam teksitine reiksme
+      h1El.textContent = vardas;
+      // patalpinu i body taga
+      document.body.append(h1El);
+
+      // pakeisti dydi
+      h1El.style.fontSize = '5rem'
+
+      //vardas = 'Mike'
+      //vardas = 500
+      //vardas = true
+
+      console.log(5 + 5);
+      console.log(vardas);
+
+      let ;
